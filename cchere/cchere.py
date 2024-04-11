815621e11231447d4d0aec62ba8bdee0620ccd9b
subhuti2 <prion.w@gmail.com>
2024-04-11 16:45:08 +0200

    omen_04/11/24_16:45:08

--- cchere/cchere.py
+++ cchere/cchere.py
@@ -28,7 +28,7 @@ def read_from_files(file_in, file_out):
     
     return usernames, output_tags
 
-def process_post_time (raw_username， output_tag)
+def process_post_time(raw_username, output_tag):
 
     # Record the start time
     start_time = datetime.datetime.now()
@@ -153,4 +153,4 @@ if usernames is None or output_tags is None:
 # Main processing loop
 for raw_username, output_tag in zip(usernames, output_tags):
 
-    process_post_time (raw_username， output_tag)
+    process_post_time (raw_username, output_tag)
